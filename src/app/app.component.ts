5327109147304c6f19c1a4dc3332e5df0a9148fb
Abhishek2149 <abhi.jaybhaye2000@gmail.com>
2025-01-15 14:43:06 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './myfirstwebsite/header/header.component';
+import { TopNavComponent } from './myfirstwebsite/top-nav/top-nav.component';
+import { LeftTextBlockComponent } from './myfirstwebsite/left-text-block/left-text-block.component';
+import { RightTextBlockComponent } from './myfirstwebsite/right-text-block/right-text-block.component';
+import { FooterComponent } from './myfirstwebsite/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,TopNavComponent,LeftTextBlockComponent,RightTextBlockComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
