08a43a3196b10dbbf13458b0bfad681d98557663
Abhishek2149 <abhi.jaybhaye2000@gmail.com>
2025-01-15 14:35:42 +0530

    added

---
+++ src/app/myfirstwebsite/left-text-block/left-text-block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-text-block',
+  imports: [],
+  templateUrl: './left-text-block.component.html',
+  styleUrl: './left-text-block.component.css'
+})
+export class LeftTextBlockComponent {
+
+}
---
+++ src/app/myfirstwebsite/right-text-block/right-text-block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-text-block',
+  imports: [],
+  templateUrl: './right-text-block.component.html',
+  styleUrl: './right-text-block.component.css'
+})
+export class RightTextBlockComponent {
+
+}
